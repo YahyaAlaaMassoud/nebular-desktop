35a8cae1e6691ddfde7e0360a51a5d3b93da8858
yahyaalaamassoud <yahyaalaamassoud@gmail.com>
2021-01-15 22:58:08 -0500

    update login page by nebular

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Validation } from '../../utils/validations';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user/user.service';
+import { HelperService } from '../../services/helper/helper.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  public loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private userService: UserService,
+              private helperService: HelperService) {
+
+  }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.compose([Validation.emailValidator, Validators.required])],
+      password: ['', Validators.compose([Validators.minLength(6), Validators.maxLength(100), Validators.required])],
+    });
+  }
+
+  async save() {
+    try {
+      await this.helperService.showLoading();
+      const result: any = await this.userService.login(this.loginForm.value);
+      this.userService.updateAndSaveCarrentUser(result);
+      this.helperService.removeLoading();
+    } catch (err) {
+      this.helperService.showError(err);
+    }
+  }
+}
---
+++ src/app/pages/login/login.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { LoginComponent } from './login.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { ComponentsModule } from '../../components/components.module';
+import { NbButtonModule, NbLayoutModule, NbSidebarModule, NbCardModule, NbInputModule, NbIconModule } from '@nebular/theme';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonicModule,
+    RouterModule.forChild(routes),
+    TranslateModule.forChild(),
+    ComponentsModule,
+    NbLayoutModule,
+    NbSidebarModule.forRoot(),
+    NbInputModule,
+    NbButtonModule,
+    NbCardModule,
+    NbIconModule
+  ],
+  declarations: [LoginComponent],
+})
+export class LoginPageModule {}
