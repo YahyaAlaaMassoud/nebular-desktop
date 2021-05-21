046c48b621dc5bfc17a0a1d547f97e3cd3faa311
yahyaalaamassoud <yahyaalaamassoud@gmail.com>
2021-05-21 15:49:55 -0400

    add EditProfile page for the specialist (user)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule) },
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule) },
+  { path: 'edit-profile', loadChildren: () => import('./pages/edit-profile/edit-profile.module').then(m => m.EditProfilePageModule) },
   { path: 'patient/:id', loadChildren: () => import('./pages/patient/patient.module').then(m => m.PatientPageModule) },
   { path: 'patient/:patient_id/modules/:module_id/charts', loadChildren: () => import('./pages/charts/charts.module').then(m => m.ChartsComponentModule) },
   { path: '', redirectTo: 'login', pathMatch: 'full'}
